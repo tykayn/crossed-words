518f283ea7d48b64ccee1f66782ed2f0a17cf72d
TyKayn <tykayn@gmail.com>
2013-11-25 13:57:25 +0100

    yeoman init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('crossedWordsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
