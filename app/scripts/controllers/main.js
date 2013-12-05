f4c14e7904c02b5f4726105ac0b1d72976c5b6f2
tykayn <tykayn@gmail.com>
2013-12-05 13:58:32 +0100

    largeur de grille éditable

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,29 +5,57 @@ angular.module('crossedWordsApp')
     
     // chaine de guidage
     $scope.chain = "abcdefgh"; 
+    $scope.side = 50; 
     //décodage de la chaine de guidage pour reconstruire la grille
     $scope.upChain = function(){
        var tab = $scope.chain.split('');
        $scope.cases_exemple = {};
        for( var i=0 ; i < ($scope.crossbox.width * $scope.crossbox.height ) ; i++){
-           $scope.cases_exemple[i] = {
+           
+           if(tab[i] != '' ){
+               if(tab[i] != undefined ){
+                  $scope.cases_exemple[i] = {
+               id: i,
                row: i,
-               col : 1,
-               content : tab[i],
+               col : i/$scope.crossbox.width ,
+               content :  tab[i] ,
                word : 1
+                } 
+               }
+               else{
+                   $scope.cases_exemple[i] = {
+               id: i,
+               row: i,
+               col : i,
+               content : '' ,
+               word : 2
+               }
+              
+            }     
            }
+           
+           
        }
     }; 
-    $scope.crossbox = {width : 10 , height : 10};
+    //décodage de la chaine de guidage pour reconstruire la grille
+    $scope.upCase = function(){
+       
+       
+    }; 
+    $scope.highLight = function(numWord){
+       $('.word_'+numWord).addClass('highLight');
+    }; 
+    $scope.downLight = function(numWord){
+       $('.word_'+numWord).removeClass('highLight');
+    }; 
+    $scope.crossbox = {width : 5 , height : 5};
     
     $scope.mots = 1;
     $scope.cases_exemple = [
-        {row: 1, col: 1, content: 'a', word:1},
-        {row: 1, col: 2, content: 'b', word:1},
-        {row: 1, col: 3, content: 'c', word:1},
-        {row: 1, col: 4, content: 'd', word:1},
-        {row: 1, col: 5, content: 'e', word:1}];
-    
-    
-    
+        {id:1, row: 1, col: 1, content: 'a', word:1},
+        {id:2, row: 1, col: 2, content: 'b', word:1},
+        {id:3, row: 1, col: 3, content: 'c', word:1},
+        {id:4, row: 1, col: 4, content: 'd', word:1},
+        {id:5, row: 1, col: 5, content: 'e', word:1}];
+
   });
